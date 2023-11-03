8947387d6f00ce798e5e39af5c133c34baa0d146
JohanLundberg12 <johanlundberg1993@gmail.com>
2023-11-03 18:01:04 +0100

    undo

--- dvc-experiment/.venv/lib/python3.8/site-packages/dvc/__pyinstaller/hook-dvc_task.py
+++
@@ -1,5 +0,0 @@
-# ruff: noqa: N999
-# pylint: disable=import-error
-from PyInstaller.utils.hooks import collect_submodules
-
-hiddenimports = collect_submodules("dvc_task")
--- dvc-experiment/.venv/lib/python3.8/site-packages/dvc/testing/benchmarks/cli/commands/test_gc.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable=unused-argument
-def test_gc(bench_dvc, tmp_dir, dvc, make_dataset):
-    make_dataset(files=False, cache=True, dvcfile=False)
-    bench_dvc("gc", "-f", "-w")
