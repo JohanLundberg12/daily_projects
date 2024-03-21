5c6c6db37ecb73ecbe6d335557661f5576a68ea4
JohanLundberg12 <johanlundberg1993@gmail.com>
2024-03-21 23:00:56 +0100

    comment

--- plotting_tricks/plot_zoom.py
+++ plotting_tricks/plot_zoom.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # x from -10 to 10
-x = np.linspace(-10, 10, 10000000)
+x = np.linspace(-10, 10, 100)
 
 # generate y as a random walk
 
@@ -16,9 +16,13 @@ for i in range(1, len(x)):
 ax = plt.axes()
 ax.plot(x, y)
 
-axin = ax.inset_axes([0.5, 0.5, 0.3, 0.3])
+# (x, y) =  position of the zoomed plot
+# width, height = width and height of the zoomed plot as percentage of the original plot
+axin = ax.inset_axes([0.8, 0.1, 0.3, 0.3])
 axin.plot(x, y)
-axin.set_xlim(-1, 1)
+
+# no y_lim, as we want to ensure the line is visible
+axin.set_xlim(-1, 1)  # focus on the region between -1 and 1 on x-axis
 
 ax.indicate_inset_zoom(axin)
 plt.show()
