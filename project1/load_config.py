4be7428af01ec8b136227444a4e3dda6b623505c
JohanLundberg12 <johanlundberg1993@gmail.com>
2024-03-26 14:16:21 +0100

    config management using hydra

---
+++ project1/load_config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+
+@dataclass
+class DataSetConfig:
+    name: str
+    path: str
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def process_config(cfg: DictConfig) -> None:
+    """Hierarchical configuration from multiple sources.
+
+    Args:
+        cfg (DictConfig): Main config file.
+    """
+    print(OmegaConf.to_yaml(cfg))
+
+    # check if the config is valid using dataclass
+    DataSetConfig(**cfg.dataset)
+
+
+if __name__ == "__main__":
+    process_config()
