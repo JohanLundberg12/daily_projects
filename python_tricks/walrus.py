36f42756bdd5b32d688082f3786eae2927b67011
JohanLundberg12 <johanlundberg1993@gmail.com>
2024-03-26 19:49:30 +0100

    refactor

--- python_tricks/walrus.py
+++ python_tricks/walrus.py
@@ -1,22 +1,42 @@
-x = 5
+import sys
+
+
+
+# walrus in an if statement
+def check_is_single_digit(number):
+    if is_single_digit:= len(list(str(number))) == 1:
+        print(f"{number} is a single digit: {is_single_digit}")
+    else:
+        print(f"{number} is not a single digit: {is_single_digit}")
 
 
 # walrus to assign result of operation in a foor loop or if statement
-for i in range(0, y := x**2):
-
-    print(i)
-
-
-if n := (len(list(str(x))) == 1):
-    print(f"Length of list(str({x})) is 1")
+def squared_walrus_for_loop(number):
+    for i in range(0, y:= number**2):
+        print(y)
 
 
-def check_x(x):
-    return x % 2
+def number_modulo_2(number):
+    return number % 2
 
 
 # Use walrus operator in a list comprehension
-y = x**2
-modulo = [True if (v := check_x(i)) == 0 else "not zero" for i in range(0, y)]
+def list_comp_with_walrus(number):
+    y = int(number) ** 2
+    modulo = [True if (v := number_modulo_2(i)) == 0 else "not zero" for i in range(0, y)]
 
-print(modulo)
+    return modulo
+
+
+if __name__ == "__main__":
+    number = int(sys.argv[1])
+
+    check_is_single_digit(number)
+
+    squared_walrus_for_loop(number)
+
+    print(list_comp_with_walrus(number))
+
+
+
+    
